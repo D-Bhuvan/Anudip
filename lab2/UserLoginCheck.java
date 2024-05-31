cc6ec84c87e1e43ab0df741c73783edf28dc7b86
D-Bhuvan <157099289+D-Bhuvan@users.noreply.github.com>
2024-05-31 13:27:39 +0530

    lab-2

---
+++ lab2/UserLoginCheck.java
@@ -0,0 +1,14 @@
+package Samplepackage;
+
+public class UserLoginCheck {
+
+	    public static void main(String[] args){
+	         String login = "Ratan";
+	         if(login == "raj") {
+	        	 System.out.println("user is valid");
+	         }
+	         else {
+	        	 System.out.println("user is not logged in");
+	         }
+	    }
+}
