f5a1e59d785db715676ffd6b557b504b4cf15290
D-Bhuvan <157099289+D-Bhuvan@users.noreply.github.com>
2024-06-12 09:00:34 +0530

    Update Swapaddnumbers.java

--- lab1/Swapaddnumbers.java
+++ lab1/Swapaddnumbers.java
@@ -1,5 +1,20 @@
 package Samplepackage;
 
 public class Swapaddnumbers {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	     int x=5;
+	     int y=9,sum;
+	    		 System.out.println("Before swapping:"+"x="+x+",y="+y);
+	     x=x+y;
+	     y=x-y;
+	     x=x-y;
+	     System.out.println("After swapping:"+"x="+x+",y="+y);
+	     sum=x+y;
+	     System.out.println("The sum of two numbers is:"+sum);
+	   
+	     
+	}
 
 }
