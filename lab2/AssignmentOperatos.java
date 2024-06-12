24913b66b74209fb70445e235f4baa975ba76908
D-Bhuvan <157099289+D-Bhuvan@users.noreply.github.com>
2024-06-12 09:14:20 +0530

    lab-2

--- lab2/AssignmentOperatos.java
+++ lab2/AssignmentOperatos.java
@@ -1,5 +1,12 @@
 package Samplepackage;
 
 public class AssignmentOperatos {
+	    public static void main(String[] args) {
+	        int num1 = 20;
+	        char letter = 'P';
+	        System.out.println("Simple assignment:");
+	        System.out.println("num1 = " + num1);
+	        System.out.println("letter = " + letter);
 
-}
+	    }
+	}
